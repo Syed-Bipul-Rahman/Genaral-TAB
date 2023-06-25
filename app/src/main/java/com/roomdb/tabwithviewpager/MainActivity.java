1fe95bf9250fe5234dbdd26fdf4c12cfa91ea6a8
Sb-Rahman <sb.rahman@yahoo.com>
2023-06-25 12:01:19 +0600

    Tablayout and viewpager added

--- app/src/main/java/com/roomdb/tabwithviewpager/MainActivity.java
+++ app/src/main/java/com/roomdb/tabwithviewpager/MainActivity.java
@@ -1,14 +1,81 @@
 package com.roomdb.tabwithviewpager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    TabLayout tab;
+    ViewPager vp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//initialize
+        tab = findViewById(R.id.tabs);
+        vp = findViewById(R.id.viewpager);
+
+        //setup
+
+        setupViewPager(vp);
+        tab.setupWithViewPager(vp);
+
+    }
+
+    private void setupViewPager(ViewPager vp) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Fragment(), "First");
+        adapter.addFragment(new Fragment(), "Second");
+        adapter.addFragment(new Fragment(), "Third");
+
+
+        vp.setAdapter(adapter);
+
+    }
+
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            //super.getPageTitle(position);
+            return fragmentTitleList.get(position);
+        }
     }
 }
